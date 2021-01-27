bd73c500be7b2b05bd7f9daf312efa602800708d
jmeza <jhonatan.meza01@gmail.com>
2021-01-27 14:55:19 +0100

    base/creations modules and material

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'client',
+    loadChildren: () => import('./modules/client/client.module').then(m => m.ClientModule)
+  },
+  {
+    path: 'incidence',
+    loadChildren: () => import('./modules/incidence/incidence.module').then(m => m.IncidenceModule)
+  },
+  {
+    path: 'proyect',
+    loadChildren: () => import('./modules/proyect/proyect.module').then(m => m.ProyectModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
